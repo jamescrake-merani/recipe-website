70dd8dfa92489b7e05b0fb01d7d9332ff6700f2d
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2024-03-07 15:55:46 +0000

    Added the tag table.

--- src/recipe_website/model.clj
+++ src/recipe_website/model.clj
@@ -3,8 +3,15 @@
 
 (def recipe-table
   (sql/format
-   {:create-table "recipe"
+   {:create-table :recipe
     :with-columns [[:id :primary-key :auto-increment]
                    [:name :text]
                    [:time-guidance :int]
                    [:published :boolean]]}))
+
+(def tag
+  (sql/format
+   {:create-table :tag
+    :with-columns [[:recipe-id :int :foreign-key [:references :recipe :id]]
+                   [:name :text]
+                   [[:primary-key [:recipe-id :name]]]]}))
