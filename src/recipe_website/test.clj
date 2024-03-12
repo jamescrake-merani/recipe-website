741c58d4296a19c11e1696bbfc2cc545672f6645
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2024-03-12 14:59:55 +0000

    Lol test recipe to make cement.

---
+++ src/recipe_website/test.clj
@@ -0,0 +1,16 @@
+(ns recipe-website.test
+  (:require  [recipe-website.model :as model]))
+
+
+(def test-recipe
+  {:name "Cement"
+   :time-guidance 60
+   :published true
+   :lines ["Purchase or collect limestone"
+           "Break the limestone into small pieces"
+           "Cook the limestone in a kiln or outdoor even"
+           "Let he baked limestone cool"
+           "Crumble the baked limestone chunks"]})
+
+
+(defn populate-database)
