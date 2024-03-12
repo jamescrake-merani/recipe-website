dc0b66c58bafa1708b5b6b20a94c03ad96f2f902
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2024-03-12 13:36:43 +0000

    Added function to add recipes to the database.

--- src/recipe_website/model.clj
+++ src/recipe_website/model.clj
@@ -34,6 +34,23 @@
    (sql/format
     {:select :* :from :recipe :where [:= :id recipe-id]})))
 
+;; TODO Insert the lines as well.
+(defn insert-recipe [recipe]
+  "RECIPE is represented as a map with the following keys
+:name
+:time-guidance
+:published
+:lines (vector of lines assumed to be in order) as follows:
+
+:conten
+"
+  (jdbc/execute-one!
+   (sql/format
+    {:insert-into :recipe
+     :columns [:name [:name :time-guidance :published]]
+     :values [(:name recipe) (:time-guidance recipe) (:published recipe)]})
+   {:return-keys true}))
+
 (defn create-tables [db]
   (map #(jdbc/execute-one! db %)
        [recipe-table tag recipe-line]))
