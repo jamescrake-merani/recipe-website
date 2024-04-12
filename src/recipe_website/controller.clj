8191be6289f11c8c50d0292b4a96a9d7a5a66335
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2024-04-12 08:03:34 +0100

    Added a recipe controller.

--- src/recipe_website/controller.clj
+++ src/recipe_website/controller.clj
@@ -1,7 +1,8 @@
 (ns recipe-website.controller
   (:require [reitit.ring :as ring]
             [recipe-website.views :as views]
-            [ring.util.http-response :as response]))
+            [ring.util.http-response :as response]
+            [recipe-website.model :as model]))
 
 (defn html-response [content]
   (-> (str content)
@@ -11,8 +12,15 @@
 (defn home-controller [req]
   (html-response (views/home-page)))
 
+(defn recipe-controller [req]
+  (html-response
+   (views/recipe-page
+    recipe
+    (model/get-recipe (:recipe-id (get-in req [:path-params :id]))))))
+
 (defn creater-handler [db]
   (ring/ring-handler
    (ring/router
-    [["/" {:get home-controller}]]
+    [["/" {:get home-controller}]
+     ["/recipe/:id" {:get recipe-controller {:parameters {:path {:id int?}}}}]]
     {:data {:db db}})))
