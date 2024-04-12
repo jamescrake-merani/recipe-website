03cba32cbbaa0ac9cc4f9743ce88a77fca61cf2b
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2024-04-12 08:23:07 +0100

    Separate handler into key.

--- src/recipe_website/core.clj
+++ src/recipe_website/core.clj
@@ -7,14 +7,18 @@
 
 (def config
   {::http-server {:port 8080
-                  :db (ig/ref ::db-connection)}
-   ::db-connection {:db-file "data.db"}})
+                  :handler {ig/ref ::app-handler}}
+   ::db-connection {:db-file "data.db"}
+   ::app-handler {:db (ig/ref ::db-connection)}})
 
 (defmethod ig/init-key ::db-connection [_ {:keys [db-file]}]
   (jdbc/get-connection {:dbtype "sqlite" :dbname db-file}))
 
-(defmethod ig/init-key ::http-server [_ {:keys [port db]}]
-  (jetty/run-jetty (defaults/wrap-defaults (controller/creater-handler db) defaults/site-defaults)
+(defmethod ig/init-key ::app-handler [_ {:keys [db]}]
+  (controller/creater-handler db))
+
+(defmethod ig/init-key ::http-server [_ {:keys [port handler]}]
+  (jetty/run-jetty (defaults/wrap-defaults handler defaults/site-defaults)
                    {:join? false
                     :port port}))
 
@@ -24,4 +28,5 @@
 (comment
   (def system
     (ig/init config))
+  (def db (::db-connection system))
   (ig/halt! system))
