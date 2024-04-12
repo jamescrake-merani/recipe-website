97fb71958af466d6a187ad98958d96bf8481c552
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2024-04-12 08:05:16 +0100

    Fixed handler.

--- src/recipe_website/controller.clj
+++ src/recipe_website/controller.clj
@@ -15,12 +15,12 @@
 (defn recipe-controller [req]
   (html-response
    (views/recipe-page
-    recipe
     (model/get-recipe (:recipe-id (get-in req [:path-params :id]))))))
 
 (defn creater-handler [db]
   (ring/ring-handler
    (ring/router
     [["/" {:get home-controller}]
-     ["/recipe/:id" {:get recipe-controller {:parameters {:path {:id int?}}}}]]
+     ["/recipe/:id" {:get {:parameters {:path {:id int?}}
+                           :handler recipe-controller }}]]
     {:data {:db db}})))
